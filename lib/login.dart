ba83caa35ff2e6578b5918df9c1a03cbcef7eb4d
alan <alancleri@gmail.com>
2022-12-16 18:50:13 -0300

    mejora en el formulario de login local

--- lib/login.dart
+++ lib/login.dart
@@ -39,6 +39,7 @@ class LocalLogin extends StatefulWidget {
 class _LocalLoginState extends State<LocalLogin> {
   late TextEditingController _controller;
   late List<DatoDB> datoDB;
+  late String textoinicial;
 
   @override
   void initState() {
@@ -48,50 +49,56 @@ class _LocalLoginState extends State<LocalLogin> {
 
   @override
   Widget build(BuildContext context) {
-    final String textoinicial = 'Insertar clave';
-    return Column(
-      children: [
-        TextField(
-          cursorColor: Colors.black,
-          style: const TextStyle(color: Colors.black),
-          decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.grey,
-              border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(50)),
-              hintText: textoinicial,
-              hintStyle: const TextStyle(color: Colors.black)),
-          controller: _controller,
-          onSubmitted: (String valuet) {
-            // textoescrito = valuet;
-          },
-        ),
-        TextButton(
-          onPressed: () {
-            Db.instance.insert(
-                DatoDB(id: 1, autologin: 'si', clave: _controller.text));
-            widget.manager.initializeMQTTClient(_controller.text);
-            widget.manager.connect();
-          },
-          child: Text(
-            'Conectar',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        TextButton(
-          onPressed: () {
-            // if (datoDB.isNotEmpty) {
-            Db.instance.delete(1);
-            // }
-          },
-          child: Text(
-            'Borra',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-      ],
-    );
+    return FutureBuilder(
+        future: Db.instance.getAllItems(),
+        builder: (BuildContext context, AsyncSnapshot<List<DatoDB>> snapshot) {
+          if (snapshot.hasData) {
+            datoDB = snapshot.data!;
+            if (datoDB.isNotEmpty) {
+              textoinicial = datoDB[0].toMap()['clave'];
+            } else {
+              textoinicial = 'Insertar clave';
+            }
+          } else {
+            textoinicial = 'Insertar clave';
+          }
+          return Column(
+            children: [
+              Text(
+                'Clave',
+                style: TextStyle(color: Colors.white),
+              ),
+              TextField(
+                cursorColor: Colors.black,
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey,
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(40)),
+                    hintText: textoinicial,
+                    hintStyle: const TextStyle(color: Colors.black)),
+                controller: _controller,
+                onSubmitted: (String valuet) {
+                  // textoescrito = valuet;
+                },
+              ),
+              TextButton(
+                onPressed: () {
+                  Db.instance.insert(
+                      DatoDB(id: 1, autologin: 'si', clave: _controller.text));
+                  widget.manager.initializeMQTTClient(_controller.text);
+                  widget.manager.connect();
+                },
+                child: Text(
+                  'Conectar',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
 
